b9daec0add10ce99800bbb4eeb77c40bc8b3e532
Lee WonJoong <realjoong@gmail.com>
2021-05-03 11:49:23 +0900

    View Model 추가

--- Android_Kotlin_Fundamentals_Gihub_files/GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ Android_Kotlin_Fundamentals_Gihub_files/GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -17,11 +17,13 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
 
@@ -29,6 +31,9 @@ import com.example.android.guesstheword.databinding.GameFragmentBinding
  * Fragment where the game is played
  */
 class GameFragment : Fragment() {
+
+    // ViewModel과 연결
+    private lateinit var viewModel: GameViewModel
 
     // The current word
     private var word = ""
@@ -51,6 +56,10 @@ class GameFragment : Fragment() {
                 container,
                 false
         )
+
+        // ViewModel 호출, GameFragment context와 GameViewModel class를 넘겨준다.
+        Log.i("GameFragment", "Called ViewModelProvider.get")
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
         resetList()
         nextWord()
