6c45a476ae534113e4a862036978d68601479a8d
Lee WonJoong <realjoong@gmail.com>
2021-06-16 00:06:31 +0900

    Modify view with alignment and arrangement

--- JetpackCompose/StevdzaSan/ComposeTest/app/src/main/java/com/example/composetest/MainActivity.kt
+++ JetpackCompose/StevdzaSan/ComposeTest/app/src/main/java/com/example/composetest/MainActivity.kt
@@ -1,21 +1,19 @@
 package com.example.composetest
 
-import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composetest.ui.theme.ComposeTestTheme
-import com.example.composetest.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,11 +22,34 @@ class MainActivity : ComponentActivity() {
             ComposeTestTheme { // this function is from Theme.kt
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    DefaultPreview()
+                    Row(
+                        modifier = Modifier
+                            .height(500.dp)
+                            .width(500.dp)
+                            .background(Color.LightGray),
+                        horizontalArrangement = Arrangement.Start,
+                        //horizontalAlignment = Alignment.CenterHorizontally, // horizontal alignment
+                        //verticalArrangement = Arrangement.Bottom, // vertical alignment
+                    ) {
+                        CustomItem(3f, color=MaterialTheme.colors.secondary)
+                        CustomItem(1f)
+                    }
                 }
             }
         }
     }
+}
+
+// default color as primary color
+@Composable
+fun RowScope.CustomItem(weight: Float, color: Color = MaterialTheme.colors.primary) {
+    Surface(
+        modifier = Modifier
+            .width(50.dp)
+            .height(50.dp)
+            .weight(weight), // To add weight, ColumnScope에서 가져와서 CustomItem을 써야함. 내장함수로
+        color = color
+    ) {}
 }
 
 // When there is "Preview" annotation, it allow us to see composable inside the right split screen
@@ -38,25 +59,13 @@ fun DefaultPreview() {
     ComposeTestTheme {
         // fillMaxSize will make the screen as big as the parent's size.
         // In this case, the parent's size is the size of screen.
-        Column(
+        Row(
             modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally, // horizontal alignment
+            horizontalArrangement = Arrangement.Start, // horizontal alignment
             //verticalArrangement = Arrangement.Bottom, // vertical alignment
         ) {
-            Surface(
-                modifier = Modifier
-                    .width(200.dp)
-                    //.height(50.dp)
-                    .weight(3f), // take place by 3/4
-                color = MaterialTheme.colors.secondary
-            ) {}
-            Surface(
-                modifier = Modifier
-                    .width(200.dp)
-                    //.height(50.dp)
-                    .weight(1f),// take place by 1/4
-                color = MaterialTheme.colors.primary
-            ) {}
+            CustomItem(3f, color=MaterialTheme.colors.secondary)
+            CustomItem(1f)
         }
     }
 }
