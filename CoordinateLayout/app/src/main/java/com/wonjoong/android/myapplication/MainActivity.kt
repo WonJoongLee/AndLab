ab66dab6c5b6ff0bca0f244216150152e198b12d
Lee WonJoong <realjoong@gmail.com>
2021-08-05 09:24:41 +0900

    Add Jetpack Compose Practice1 Project

--- CoordinateLayout/app/src/main/java/com/wonjoong/android/myapplication/MainActivity.kt
+++ CoordinateLayout/app/src/main/java/com/wonjoong/android/myapplication/MainActivity.kt
@@ -1,11 +1,21 @@
-    package com.wonjoong.android.myapplication
+package com.wonjoong.android.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.wonjoong.android.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btScrollingActivity.setOnClickListener {
+            val intent = Intent(this, ScrollingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ JetpackCompose/ComposePractice1/app/src/main/java/com/wonjoong/android/composepractice1/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.wonjoong.android.composepractice1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.wonjoong.android.composepractice1.ui.theme.ComposePractice1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard("Android")
+        }
+    }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard(){
+    MessageCard("Android")
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposePractice1Theme {
+        Greeting("Android")
+    }
+}
