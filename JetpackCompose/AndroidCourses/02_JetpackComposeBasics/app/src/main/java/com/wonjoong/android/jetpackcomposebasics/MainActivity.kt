5194f8009deec12b019508ce34604e3c4d2a94eb
Lee WonJoong <realjoong@gmail.com>
2021-08-07 10:44:58 +0900

    done "State in Compose"

--- JetpackCompose/AndroidCourses/02_JetpackComposeBasics/app/src/main/java/com/wonjoong/android/jetpackcomposebasics/MainActivity.kt
+++ JetpackCompose/AndroidCourses/02_JetpackComposeBasics/app/src/main/java/com/wonjoong/android/jetpackcomposebasics/MainActivity.kt
@@ -3,36 +3,75 @@ package com.wonjoong.android.jetpackcomposebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.wonjoong.android.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeBasicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MyScreenContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(content: @Composable () -> Unit) {
+    JetpackComposeBasicsTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+            content()
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+    Button(onClick = { updateCount(count + 1) }) {
+        Text("I've been clicked $count times")
+    }
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = listOf("Android", "there", "Wonjoong", "Nice", "great")) {
+
+    val counterState = remember { mutableStateOf(0) }
+
+    Surface(color = Color.Yellow) {
+        Column {
+            for (name in names) {
+                Greeting(name = name)
+                Divider(color = Color.Black)
+            }
+            Divider(color = Color.Transparent, thickness = 32.dp)
+            Counter(count = counterState.value,
+                updateCount = { newCount -> counterState.value = newCount })
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color = Color.Yellow) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
+}
+
+@Preview(showBackground = true, name = "Text preview")
 @Composable
 fun DefaultPreview() {
-    JetpackComposeBasicsTheme {
-        Greeting("Android")
+    MyApp {
+        MyScreenContent()
     }
 }
