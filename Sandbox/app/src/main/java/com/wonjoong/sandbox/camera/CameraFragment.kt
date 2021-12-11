11aae086a757af5162c66a5e5380065e8826c896
Lee WonJoong <realjoong@gmail.com>
2021-12-11 18:40:24 +0900

    글자 인식 기능 추가

--- Sandbox/app/src/main/java/com/wonjoong/sandbox/camera/CameraFragment.kt
+++ Sandbox/app/src/main/java/com/wonjoong/sandbox/camera/CameraFragment.kt
@@ -1,15 +1,24 @@
 package com.wonjoong.sandbox.camera
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.tasks.Task
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import com.wonjoong.sandbox.R
 import com.wonjoong.sandbox.databinding.FragmentCameraBinding
 import com.wonjoong.sandbox.util.BaseFragment
@@ -41,26 +50,78 @@ class CameraFragment : BaseFragment<FragmentCameraBinding>(R.layout.fragment_cam
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(requireActivity())
         cameraProviderFuture.addListener({
+            // lifecycleowner에 맞춰 camera lifecycle을 맞추기 위해 추가하는 코드
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            // 프리뷰
             val preview = Preview.Builder()
                 .build()
                 .also {
                     it.setSurfaceProvider(viewFinder.surfaceProvider)
                 }
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, CustomImageAnalyzer(this::setRecognizedText))
+                }
+            // 후면 카메라 Default 설정
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
             try {
+                // 다시 바인딩 하기 전에 unbind
                 cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(requireActivity(), cameraSelector, preview)
+                // 카메라 바인딩
+                cameraProvider.bindToLifecycle(
+                    requireActivity(),
+                    cameraSelector,
+                    preview,
+                    imageAnalyzer
+                )
             } catch (exc: Exception) {
                 Log.e("Camera Fragment", "Use case binding failed", exc)
             }
+            // 메인 쓰레드에서 도는 Executor 설정
         }, ContextCompat.getMainExecutor(requireActivity()))
+    }
+
+    private fun setRecognizedText(recognizedText: String) {
+        lifecycleScope.launchWhenStarted {
+            binding.tvResult.text = recognizedText
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         cameraExecutor.shutdown()
     }
+
+    private class CustomImageAnalyzer(
+        private val setRecognizedText: (String) -> Unit
+    ) : ImageAnalysis.Analyzer {
+        private val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+        @SuppressLint("UnsafeOptInUsageError")
+        override fun analyze(image: ImageProxy) {
+            val mediaImage = image.image
+            if (mediaImage != null) {
+                val newImage =
+                    InputImage.fromMediaImage(mediaImage, image.imageInfo.rotationDegrees)
+                detectInImage(newImage)
+                    .addOnSuccessListener { newRecognizedText ->
+                        setRecognizedText(newRecognizedText.text)
+                    }
+                    .addOnFailureListener {
+                        Log.e("onFailure", "exception : $it")
+                    }
+                    .addOnCompleteListener {
+                        image.close()
+                    }
+            }
+        }
+
+        private fun detectInImage(image: InputImage): Task<Text> {
+            return recognizer.process(image)
+        }
+    }
+
 
     companion object {
         private const val TAG = "CameraXBasic"
