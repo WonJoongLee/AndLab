ea67079385e2491fa75164df740cf7b1f194d93f
Lee WonJoong <realjoong@gmail.com>
2022-02-11 08:34:55 +0900

    api 가져오는 코드 추가

--- ComposeForDesktop/SunnyDesk/src/main/kotlin/Repository.kt
+++ ComposeForDesktop/SunnyDesk/src/main/kotlin/Repository.kt
@@ -44,28 +44,41 @@ import io.ktor.client.features.json.serializer.*
 import io.ktor.client.request.*
 
 class Repository(
-  private val apiKey: String,
-  private val client: HttpClient = defaultHttpClient,
+    private val apiKey: String,
+    private val client: HttpClient = defaultHttpClient,
 ) {
-  suspend fun getWeatherForCity(city: String): WeatherResponse =
-    client.get(
-      "https://api.weatherapi.com/v1/forecast.json" +
-          "?key=$apiKey" +
-          "&q=$city" +
-          "&days=5" +
-          "&aqi=no" +
-          "&alerts=no"
-    )
+    private val transformer = WeatherTransformer()
 
-  companion object {
-    val defaultHttpClient = HttpClient(CIO) {
-      install(JsonFeature) {
-        serializer = KotlinxSerializer(
-          json = kotlinx.serialization.json.Json {
-            ignoreUnknownKeys = true
-          }
-        )
-      }
+    suspend fun weatherForCity(city: String): Lce<WeatherResults> {
+        return try {
+            val result = getWeatherForCity(city)
+            val content = transformer.transform(result)
+            Lce.Content(content)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Lce.Error(e)
+        }
     }
-  }
+
+    private suspend fun getWeatherForCity(city: String): WeatherResponse =
+        client.get(
+            "https://api.weatherapi.com/v1/forecast.json" +
+                    "?key=$apiKey" +
+                    "&q=$city" +
+                    "&days=5" +
+                    "&aqi=no" +
+                    "&alerts=no"
+        )
+
+    companion object {
+        val defaultHttpClient = HttpClient(CIO) {
+            install(JsonFeature) {
+                serializer = KotlinxSerializer(
+                    json = kotlinx.serialization.json.Json {
+                        ignoreUnknownKeys = true
+                    }
+                )
+            }
+        }
+    }
 }
