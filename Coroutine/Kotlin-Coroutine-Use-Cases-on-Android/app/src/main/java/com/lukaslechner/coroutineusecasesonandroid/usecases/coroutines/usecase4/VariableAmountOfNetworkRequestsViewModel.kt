01fa100ad026217109b370e1bc6a519034b3afdb
Lee WonJoong <realjoong@gmail.com>
2022-02-19 13:28:20 +0900

    section5 완료

--- Coroutine/Kotlin-Coroutine-Use-Cases-on-Android/app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase4/VariableAmountOfNetworkRequestsViewModel.kt
+++ Coroutine/Kotlin-Coroutine-Use-Cases-on-Android/app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase4/VariableAmountOfNetworkRequestsViewModel.kt
@@ -1,17 +1,49 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase4
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class VariableAmountOfNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequestsSequentially() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            try {
+                val recentVersions = mockApi.getRecentAndroidVersions()
+                val versionFeatures = recentVersions.map { androidVersion ->
+                    mockApi.getAndroidVersionFeatures(androidVersion.apiLevel)
+                }
+                uiState.value = UiState.Success(versionFeatures)
+            } catch (e: Exception) {
+                uiState.value = UiState.Error("Network Request failed")
+            }
+        }
     }
 
     fun performNetworkRequestsConcurrently() {
-
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            try {
+                val recentVersions = mockApi.getRecentAndroidVersions()
+                val versionFeatures = recentVersions.map { androidVersion ->
+                    async {
+                        mockApi.getAndroidVersionFeatures(androidVersion.apiLevel)
+                    }
+                }.awaitAll()
+                uiState.value = UiState.Success(versionFeatures)
+                //val deferredList = recentVersions.ma
+            } catch (e: Exception) {
+                uiState.value = UiState.Error("Network Request failed")
+            }
+        }
     }
 }
