48b10e3500f786a687b7057299b71d1eae5b1abe
WonJoongLee <realjoong@gmail.com>
2022-02-26 18:11:17 +0900

    딥링크로 액티비티 이동 구현

--- DeepLink/DeepLinkPractice1/app/src/main/java/com/example/deeplinkpractice1/MainActivity.kt
+++ DeepLink/DeepLinkPractice1/app/src/main/java/com/example/deeplinkpractice1/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.deeplinkpractice1
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val moveBtn = findViewById<TextView>(R.id.btn_move)
+        moveBtn.setOnClickListener {
+            this.startActivity(Intent(Intent.ACTION_VIEW).apply { data = Uri.parse("isaac://deeplink/goToSecondActivity") })
+        }
     }
 }
